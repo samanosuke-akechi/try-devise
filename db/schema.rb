4f6aeb02a3a8d00888c9811e25149b97f1ec109f
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-08-11 11:18:41 +0900

    usersテーブルにusernameカラム追加
    以下のコマンドでカラム追加用のマイグレーションファイル生成。
    rails g migration AddUserNameToUsers username:string:uniq
    そのあと、「rails db:migrate」実行。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_012238) do
+ActiveRecord::Schema.define(version: 2021_08_11_021535) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 2021_08_11_012238) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
