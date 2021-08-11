09a56690e192d11a9f38959c88fa04b740ffb218
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-08-11 11:48:59 +0900

    ログイン認証の条件をオーバーライド

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
          :authentication_keys => [:login]
 
   attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
 end
